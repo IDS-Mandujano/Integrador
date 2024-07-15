20f5bd2c031d87907424381d64a89bae5fdc7f04
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-07-14 20:41:30 -0600

    CONECTED API GROUPS

--- src/components/Group/molecules/GrupoCard.jsx
+++ src/components/Group/molecules/GrupoCard.jsx
@@ -3,18 +3,18 @@ import Text from "../atoms/Text";
 import Image from "../atoms/Image";
 import Button from "../atoms/Button";
 
-function GrupoCard() {
+function GrupoCard(props) {
     return (
         <div className="flex flex-col bg-white rounded-lg shadow-md p-4 w-full h-full">
             <div className="flex ml-8  items-center h-1/4">
-                <Title text="Grupo" />
+                <Title text={props.asignatura} />
             </div>
             <div className="flex h-1/4">
-                <Text className="ml-8" text="Estudiantes de primer año IDS" />
+                <Text className="ml-8" text={`${props.grado} - ${props.grupo}`} />
             </div>
             <div className="flex items-center h-1/4 ml-8">
                 <Image image="vite.svg" />
-                <Text text="20 miembros" />
+                <Text text={`${props.miembros} - Miembros`} />
             </div>
             <div className="flex h-1/4">
                 <Button className="text-white bg-teal-800 rounded w-2/5 ml-8" text="Inspeccionar" />
--- src/components/Group/organisms/GroupContainer.jsx
+++ src/components/Group/organisms/GroupContainer.jsx
@@ -1,14 +1,44 @@
+import { useState,useEffect } from "react";
 import GrupoCard from "../molecules/GrupoCard";
 import SectionHead from "../molecules/SectionHead";
 
 function GroupContainer() {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_LOCAL_API}/grupos/`, {
+            method: 'GET',
+            headers: {
+                'x-access-token': `${import.meta.env.VITE_TOKEN_PRUEBAS}`
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('No fue posible establer comunicación con el server');
+            }
+        })
+        .then(data => {
+            setCharacters(data || []);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }, []);
+
     return (
         <div className="flex flex-col w-full items-center p-6">
             <div className="w-full mb-6">
                 <SectionHead />
             </div>
             <div className="grid md:grid-cols-3 lg:grid-rows-3 gap-6 w-full h-svh">
-                <GrupoCard />
+                {characters.map((item,index) => 
+                <GrupoCard key={index} 
+                asignatura={item.Asignatura}
+                grado={item.Grado}
+                grupo={item.Grupo}
+                miembros={characters.length}/>)}
             </div>
         </div>
     );
