48e2cc76200ba04b427fbbb3f979b3b3b91a86dd
IDS-HUGO <hugofranciscoluis@gmail.com>
2024-07-16 15:26:41 -0600

    ADD HOME ALUMNO

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import GroupDetail from './pages/GroupDetail'
+import VistaInscritos from './pages/VistaInscritos'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Login/>}/>
         <Route path='/Home' element={<Home/>}/>
         <Route path='/Group/Details' element={<GroupDetail/>}/>
+        <Route path='/Home/Alumnos' element={<VistaInscritos/>}/>
       </Routes>
     </BrowserRouter>
   )
