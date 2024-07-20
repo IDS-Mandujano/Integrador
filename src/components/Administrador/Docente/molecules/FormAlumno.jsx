d0770fb2438d94eb36472c149532190e24fface3
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-07-20 00:08:42 -0600

    CONECTED ADD ALUMNO

--- src/components/Administrador/Docente/molecules/FormAlumno.jsx
+++ src/components/Administrador/Docente/molecules/FormAlumno.jsx
@@ -1,19 +1,71 @@
+import { useState } from "react";
 import Label from "../atoms/Label";
 import Button from "../atoms/Button";
 import Input from "../atoms/Input";
-import fields from "../../../../data/formData"
+import fields from "../../../../data/formData";
 
 function FormAlumno() {
+    const token = localStorage.getItem('authToken');
+
+    const [dataAlumno, setDataAlumno] = useState({
+        Matricula: '', Nombre: '', ApellidoP: '', ApellidoM: '', Edad: '', Calificacion: '', CURP: '', Contrasena: '',
+        Correo: '', Grupo: '', Grado: ''
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setDataAlumno(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleAgregarAlumno = async (e) => {
+        e.preventDefault();
+
+        const url = `${import.meta.env.VITE_LOCAL_API}/alumnos`;
+        console.log("URL de la API:", url);
+        console.log("Datos enviados:", dataAlumno);
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `${token}`
+                },
+                body: JSON.stringify(dataAlumno),
+                credentials: 'include'
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log("Respuesta del servidor:", data);
+            } else {
+                const errorData = await response.json();
+                console.error('Error en la solicitud:', errorData);
+            }
+        } catch (error) {
+            console.error('Error durante la solicitud:', error);
+        }
+    };
 
     return (
-        <form className="flex flex-wrap items-center space-x-4 p-4 bg-white rounded-lg shadow-md">
+        <form onSubmit={handleAgregarAlumno} className="flex flex-wrap items-center space-x-4 p-4 bg-white rounded-lg shadow-md">
             {fields.map((field, index) => (
-                <div key={index} className="flex flex-col mb-2">
+                <div key={index} className="flex flex-col mb-4">
                     <Label text={field.label} />
-                    <Input type={field.type} placeholder={field.placeholder} />
+                    <Input
+                        type={field.type}
+                        placeholder={field.placeholder}
+                        name={field.key}
+                        value={dataAlumno[field.key] || ''}
+                        onChange={handleInputChange}
+                        className="p-2 border border-gray-300 rounded-md"
+                    />
                 </div>
             ))}
-            <div className="mt-4">
+            <div className="mt-4 w-full">
                 <Button text="Agregar" className="w-full" />
             </div>
         </form>
--- src/data/formData.js
+++ src/data/formData.js
@@ -1,12 +1,12 @@
 const fields = [
-    { label: "Matrícula", type: "text", placeholder: "123456" },
-    { label: "Nombre(s)", type: "text", placeholder: "Jose Angel" },
-    { label: "Apellido P.", type: "text", placeholder: "Estrada" },
-    { label: "Apellido M.", type: "text", placeholder: "Estrada" },
-    { label: "Contraseña", type: "password", placeholder: "******" },
-    { label: "Correo", type: "email", placeholder: "correo@ejemplo.com" },
-    { label: "Grupo", type: "text", placeholder: "A" },
-    { label: "Grado", type: "text", placeholder: "6" }
+    { label: "Matrícula", type: "text", placeholder: "123456", key: "Matricula" },
+    { label: "Nombre(s)", type: "text", placeholder: "Jose Angel", key: "Nombre" },
+    { label: "Apellido P.", type: "text", placeholder: "Estrada", key: "ApellidoP" },
+    { label: "Apellido M.", type: "text", placeholder: "Estrada", key: "ApellidoM" },
+    { label: "Contraseña", type: "password", placeholder: "******", key: "Contrasena" },
+    { label: "Correo", type: "email", placeholder: "correo@ejemplo.com", key: "Correo" },
+    { label: "Grupo", type: "text", placeholder: "A", key: "Grupo" },
+    { label: "Grado", type: "number", placeholder: "6", key: "Grado" }
 ];
 
 export default fields;
