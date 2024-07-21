5fe9b993a7ec9368a492331b5d5e904c3ff800d5
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-07-21 13:55:42 -0600

    Modal in progress

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,25 @@
-import ContainerForm from "../components/Login/organisms/ContainerForm";
+//import ContainerForm from "../components/Login/organisms/ContainerForm";
+import DeleteModal from "../components/Templates/DeleteModal";
+import { useState } from "react";
 
 function Login(){
+    const [open,setOpen] = useState(false)
+
     return(
-        <div className="flex w-full h-screen justify-center items-center">
-            <ContainerForm/>
-        </div>
+        // <div className="flex w-full h-screen justify-center items-center">
+        //     <ContainerForm/>
+        // </div>
+        <>
+        <button 
+        onClick={()=>setOpen(true)}
+        className="m-14 flex rounded-sm justify-center p-2 w-[10%] bg-red-600 text-white">
+            Delete
+        </button>
+        <DeleteModal open={open} onClose={()=>setOpen(false)}>
+            <img src="Icons/delete.png"
+            className="size-56 mx-auto bg-red-500 rounded"/>
+        </DeleteModal>
+        </>
     )
 }
 
