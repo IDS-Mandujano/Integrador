8680232fd7a7392fef048032bb7ffb3190793cbf
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-07-22 23:16:14 -0600

    Change a want to Link

--- src/components/Header/molecules/Navbar.jsx
+++ src/components/Header/molecules/Navbar.jsx
@@ -1,12 +1,21 @@
+import { Link } from "react-router-dom";
 import Image from "../atoms/Image";
 
 function Navbar() {
     return (
         <nav className="flex w-full justify-between">
-            <a href="/Home" className="hover:opacity-75"><Image className="w-10 h-10" image="Icons/home.png" /></a>
-            <a href="#"  className="hover:opacity-75"><Image className="w-10 h-10" image="Icons/group_add.png"/></a>
-            <a href="/Agregar" className="hover:opacity-75"><Image className="w-10 h-10" image="Icons/person_add.png" /></a>
-            <a href="#" className="hover:opacity-75"><Image className="w-10 h-10" image="Icons/logout.png" /></a>
+            <Link to="/Home" className="hover:opacity-75">
+                <Image className="w-10 h-10" image="Icons/home.png" />
+            </Link>
+            <Link to="#" className="hover:opacity-75">
+                <Image className="w-10 h-10" image="Icons/group_add.png" />
+            </Link>
+            <Link to="/Agregar" className="hover:opacity-75">
+                <Image className="w-10 h-10" image="Icons/person_add.png" />
+            </Link>
+            <Link to="#" className="hover:opacity-75">
+                <Image className="w-10 h-10" image="Icons/logout.png" />
+            </Link>
         </nav>
     );
 }
